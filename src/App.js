85956be9c617b6ca1412ace4cde7e1e1f7aa5a5c
kelly r maass <kmaass77627@gmail.com>
2021-03-10 04:20:09 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import CreateTodo from './components/todos/CreateTodo'
+// import React, { Component } from 'react';
+// import CreateTodo from './components/todos/CreateTodo'
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <CreateTodo />
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CreateTodo />
-      </div>
-    );
-  }
-}
-
-export default App;
+// "A good teacher can inspire hope, ignite the imagination, and instill a love of learning." ~Brad Henry
--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
+
+import React, { Component } from 'react';
 
 class CreateTodo extends Component {
-  render() {
-    return(
-      <div>
-        Create Todo Component
-      </div>
-    )
-  }
-}
+
+ constructor() {
+   super();
+   this.state = {
+     text: '',
+   };
+ }
+
+ handleChange = event => {
+   this.setState({
+     text: event.target.value
+   });
+ }
+
+ render() {
+   return(
+     <div>
+       <form>
+         <p>
+           <label>add todo</label>
+           <input
+         type="text"
+         onChange={this.handleChange} value={this.state.text}/>
+         </p>
+         <input type="submit" />
+      </form>
+      {this.state.text}
+    </div>
+  );
+ }
+};
 
 export default CreateTodo;
